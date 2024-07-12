b0134cd30de960557be904ad05e6a594947d6678
karlpolancos <155634584+karlpolancos@users.noreply.github.com>
2024-07-12 10:57:02 +0800

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,1 +1,5 @@
+getgenv().TrackerSettings = {
+  Webhook = "<webhook url>/messages/<message id>" -- change webhook url to your copied webhook url and copy the id of the message you sent on that channel (use discohook.org to send first message)
+}
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/karlpolancos/adstatstracker/main/main.lua"))()
